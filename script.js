ba74091e7429604d53f9bdd98a97693afe893bbd
Renan Casarin Munhoes <106607259+munhoesrc@users.noreply.github.com>
2022-08-03 12:21:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,46 @@
+// Efeito máquina de escrever no topo da page
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => elemento.innerHTML += letra, 120 * i);
+  });
+}
+
+// Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
+// function typeWriter(elemento) {
+//   const textoArray = elemento.innerHTML.split('');
+//   elemento.innerHTML = '';
+//   for(let i = 0; i < textoArray.length; i++) {
+//     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
+//   }
+// }
+
+const titulo = document.querySelector('.navbar-brand');
+typeWriter(titulo);
+
+// Botão back to top
+let mybutton = document.getElementById("btn-back-to-top");
+
+// Quando o usuário rolar para baixo 20px da parte superior do documento, mostre o botão
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// Quando o usuário clicar no botão, role até o topo do documento
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
