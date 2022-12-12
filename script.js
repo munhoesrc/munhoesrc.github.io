75276da3cc03a01d355bf09f8ee5b2c8b942fc44
Renan Casarin Munhoes <munhoesrc@gmail.com>
2022-12-12 17:44:01 -0300

    feat: add new project

--- script.js
+++ script.js
@@ -7,15 +7,6 @@ function typeWriter(elemento) {
   });
 }
 
-// Se estiver tendo problemas com performance, utilize o FOR loop como abaixo no local do forEach
-// function typeWriter(elemento) {
-//   const textoArray = elemento.innerHTML.split('');
-//   elemento.innerHTML = '';
-//   for(let i = 0; i < textoArray.length; i++) {
-//     setTimeout(() => elemento.innerHTML += textoArray[i], 75 * i);
-//   }
-// }
-
 const titulo = document.querySelector('.navbar-brand');
 typeWriter(titulo);
 
